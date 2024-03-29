e3b578487cefc9950bcc4527c5b18ebf3739b456
testcase-108 <163167404+testcase-108@users.noreply.github.com>
2024-03-29 18:36:13 +0000

    learned datattpes

---
+++ 01-basics/01-data.js
@@ -0,0 +1,13 @@
+let age = 20
+let name = "Krishna khandelwal"
+let address = "123456789"
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+//undefined
+// symbvol => unique
+
+console.log(typeof age)
