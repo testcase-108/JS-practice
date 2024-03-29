c68e65c72f9f80cca9032ddb7575df5d13f02ed2
testcase-108 <163167404+testcase-108@users.noreply.github.com>
2024-03-29 18:13:02 +0000

    Changes have been commited

---
+++ 01-basics/01-var.js
@@ -0,0 +1,9 @@
+const account = 12345678
+let accountName = "Krishan Khandelwal"
+var accountPlace = "Jurhera"
+accountMod = "Jhamnagar"
+
+/* Dont ever use var , as it is an old practice, and scope errors can be occured*/
+
+console.log(account)
+console.table([account, accountName, accountPlace, accountMod])
